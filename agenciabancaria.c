7f1505108d7527eef707d94bee4c248658c60ab4
SAMIRA <111064435+samirafq@users.noreply.github.com>
2022-10-22 13:27:22 -0300

    arquivo

--- agenciabancaria.c
+++ agenciabancaria.c
@@ -8,9 +8,10 @@ struct agenciabancaria{
 
     char nome[60];
     int codigo;
+    char localizacao[60];
     int conta;
     int numero_do_cliente;
-    char hora_de_funcionamento;
+    char hora_de_funcionamento[50];
     struct agenciabancaria*prox;
     
 };
@@ -18,17 +19,21 @@ struct agenciabancaria{
 
 AgenciaBancaria* adc_conta(AgenciaBancaria* dados){
 
-AgenciaBancaria* novo = (AgenciaBancaria*) malloc(sizeof(AgenciaBancaria));
+AgenciaBancaria* agencia = (AgenciaBancaria*) malloc(sizeof(AgenciaBancaria));
 
-    printf("Digite o nome do cliente: ");
-    scanf("%s", dados->nome);
-    printf("Digite o codigo do cliente: ");
-    scanf("%d", dados->codigo);
-    printf("Digite a conta do cliente: ");
-    scanf("%d", dados->conta);
-    printf("Digite o numero do cliente: ");
-    scanf("%d", dados->numero_do_cliente);
-    printf("Digite o hoarario de funcionamento: ");
-    scanf("%s", dados->hora_de_funcionamento);
-
+    printf("Por favor, insira o nome do cliente: ");
+    scanf("%s", agencia->nome);
+    printf("Por favor, insira o codigo do cliente: ");
+    scanf("%d", agencia->codigo);
+    printf("Por favor, insira a localizacao da agencia: ");
+    scanf("%s", agencia->localizacao);
+    printf("Por favor, insira a conta do cliente: ");
+    scanf("%d", agencia->conta);
+    printf("Por favor, insira o numero do cliente: ");
+    scanf("%d", agencia->numero_do_cliente);
+    printf("Por favor, insira o hoarario de funcionamento da agencia: ");
+    scanf("%s", agencia->hora_de_funcionamento);
+   
 }
+
+
--- agenciabancaria.h
+++ agenciabancaria.h
@@ -1,7 +1,7 @@
 typedef struct agenciabancaria AgenciaBancaria;
 AgenciaBancaria* adc_conta(AgenciaBancaria* dados);
-AgenciaBancaria* remove_conta(AgenciaBancaria* dados);
-AgenciaBancaria* imprimir_contas_cadastradas(AgenciaBancaria* dados);
+AgenciaBancaria* remover_conta(AgenciaBancaria* dados);
+AgenciaBancaria* listar_contas_cadastradas(AgenciaBancaria* dados);
 AgenciaBancaria* buscar_conta(AgenciaBancaria* dados);
 AgenciaBancaria* editar_conta(AgenciaBancaria* dados);
 AgenciaBancaria* consultar_conta_ativa(AgenciaBancaria* dados);
